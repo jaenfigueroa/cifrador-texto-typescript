7577cd993ba120644594c51332fc016f660c7d6b
jaen <jaenfigueroa03042000@gmail.com>
2023-04-16 11:04:50 -0500

    feat: write only lowercase letters and spaces

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,51 @@
 import { encrypt } from './helpers/encrypt'
 import { decrypt } from './helpers/decrypt'
 
-const input:HTMLTextAreaElement | null = document.querySelector('#textarea1')
-const encryptButton:HTMLButtonElement | null = document.querySelector('#encryptButton')
-const decryptButton:HTMLButtonElement | null = document.querySelector('#decryptButton')
-const warningSection:HTMLElement | null = document.querySelector('#warningSection')
-const resultsSection:HTMLElement | null = document.querySelector('#resultsSection')
-const results:HTMLTextAreaElement | null = document.querySelector('#textarea2')
-const copyButton:HTMLButtonElement | null = document.querySelector('#copyButton')
+const input = document.querySelector('#textarea1') as HTMLTextAreaElement
+const encryptButton = document.querySelector('#encryptButton') as HTMLButtonElement
+const decryptButton = document.querySelector('#decryptButton') as HTMLButtonElement
+const warningSection = document.querySelector('#warningSection') as HTMLElement
+const resultsSection = document.querySelector('#resultsSection') as HTMLElement
+const results = document.querySelector('#textarea2') as HTMLTextAreaElement
+const copyButton = document.querySelector('#copyButton') as HTMLButtonElement
 
 // TRAER TEXTO
 const getTextValue = ():string => {
-  return input!.value
+  return input.value
 }
 
 // MOSTRAR RESULTADOS, ALTERNAR ENTRE LAS SECCIONES
 const showResults = (text:string) => {
-  results!.value = text
+  results.value = text
 
-  warningSection!.classList.toggle('novisible', !!text)
-  resultsSection!.classList.toggle('novisible', !text)
+  warningSection.classList.toggle('novisible', !!text)
+  resultsSection.classList.toggle('novisible', !text)
 }
 
 // COPIAR TEXTO
 const copyText = () => {
-  navigator.clipboard.writeText(results!.value)
+  navigator.clipboard.writeText(results.value)
 
   // ** cambiar estilos del boton
-  copyButton!.innerText = 'Texto copiado :D'
-  copyButton!.classList.add('button--copy')
+  copyButton.innerText = 'Texto copiado :D'
+  copyButton.classList.add('button--copy')
 
   setTimeout(() => {
-    copyButton!.innerText = 'Copiar'
-    copyButton!.classList.remove('button--copy')
+    copyButton.innerText = 'Copiar'
+    copyButton.classList.remove('button--copy')
   }, 1000)
 }
 
-encryptButton!.addEventListener('click', ()=>{
+// ESCUCHADORES DE EVENTOS
+encryptButton.addEventListener('click', ()=>{
   showResults(encrypt(getTextValue()))
 })
-decryptButton!.addEventListener('click', ()=>{
+decryptButton.addEventListener('click', ()=>{
   showResults(decrypt(getTextValue()))
 })
-copyButton!.addEventListener('click', copyText)
+copyButton.addEventListener('click', copyText)
+
+input.addEventListener('input', (event:Event) => {
+  const newText:string = (event.target as HTMLInputElement).value.replace(/[^a-z\s]+/g , '')
+  input.value = newText
+})
