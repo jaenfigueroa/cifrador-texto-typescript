52e8343dc7f74f11d248336cc5e9e1a466b36b55
jaen <jaenfigueroa03042000@gmail.com>
2023-04-21 11:45:36 -0500

    refactor: format code

--- src/tests/encryption.test.ts
+++ src/tests/encryption.test.ts
@@ -1,32 +1,24 @@
 import { encrypt, decrypt } from '../js/modules/encryption';
 
 test('Debe encriptar el texto', () => {
-  expect(
-    encrypt('jaenfigueroa'))
+  expect(encrypt('jaenfigueroa'))
     .toEqual('jaienternfimesgufatenterroberai')
-  expect(
-    encrypt('otorongo durmiendo'))
+  expect(encrypt('otorongo durmiendo'))
     .toEqual('obertoberroberngober dufatrmimesenterndober')
-  expect(
-    encrypt('me gusta el cafe con leche'))
+  expect(encrypt('me gusta el cafe con leche'))
     .toEqual('menter gufatstai enterl caifenter cobern lenterchenter')
-  expect(
-    encrypt('el perro corre por el parque sin cesar'))
+  expect(encrypt('el perro corre por el parque sin cesar'))
     .toEqual('enterl penterrrober coberrrenter poberr enterl pairqufatenter simesn centersair')
 })
 
 test('Debe desencriptar el texto', () => {
-  expect(
-    decrypt('jaienternfimesgufatenterroberai'))
+  expect(decrypt('jaienternfimesgufatenterroberai'))
     .toEqual('jaenfigueroa')
-  expect(
-    decrypt('obertoberroberngober dufatrmimesenterndober'))
+  expect(decrypt('obertoberroberngober dufatrmimesenterndober'))
     .toEqual('otorongo durmiendo')
-  expect(
-    decrypt('menter gufatstai enterl caifenter cobern lenterchenter'))
+  expect(decrypt('menter gufatstai enterl caifenter cobern lenterchenter'))
     .toEqual('me gusta el cafe con leche')
-  expect(
-    decrypt('enterl penterrrober coberrrenter poberr enterl pairqufatenter simesn centersair'))
+  expect(decrypt('enterl penterrrober coberrrenter poberr enterl pairqufatenter simesn centersair'))
     .toEqual('el perro corre por el parque sin cesar')
 })
 
