0a5874e1cae5b66bbb9ae65d1a2ce7f6716ec69c
jaen <jaenfigueroa03042000@gmail.com>
2023-04-23 07:00:26 -0500

    refactor: prevent dictionary mutation

--- src/ts/services/dictionary.ts
+++ src/ts/services/dictionary.ts
@@ -1,9 +1,9 @@
 import { Dictionary } from '../interfaces/Dictionary';
 
-export const DICTIONARY: Dictionary = {
+export const DICTIONARY: Dictionary = Object.freeze({
   e: 'enter',
   i: 'imes',
   o: 'ober',
   a: 'ai',
   u: 'ufat'
-}
+})
